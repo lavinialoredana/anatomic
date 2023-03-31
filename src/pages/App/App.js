98c7e193836550daa86903643cd026d456ab34df
lavinia_esp <lavinialoreda@gmail.com>
2023-03-31 10:29:56 +0200

    Create HeaderBar component and styles

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,12 +1,11 @@
-import logoAnatomic from "../../images/logoAnatomic.jpeg";
+import HeaderBar from "../../components/HeaderBar";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logoAnatomic} className="App-logo" alt="logo" />
-                <span> Hearts full of bits </span>
+                 <HeaderBar />
             </header>
         </div>
     );
