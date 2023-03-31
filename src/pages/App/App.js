564a5ecafe449bc1b719cd2399181e71aa903963
lavinia_esp <lavinialoreda@gmail.com>
2023-03-31 15:09:55 +0200

    CSS refactor + Card container

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,8 +5,91 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                 <HeaderBar />
+                <HeaderBar />
             </header>
+
+            <main className="Card-container">
+                <article>
+                    <header>
+                        <h5> Album</h5>
+                        <p> Artist Name</p>
+                    </header>
+                    <img
+                        src="https://static.tvtropes.org/pmwiki/pub/images/350x350px-7748e070_axisboldaslove_950.jpeg"
+                        alt=""
+                    />
+                    <footer>
+                        <button> Add to Albums </button>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <h5> Album</h5>
+                        <p> Artist Name</p>
+                    </header>
+                    <img
+                        src="https://static.tvtropes.org/pmwiki/pub/images/350x350px-7748e070_axisboldaslove_950.jpeg"
+                        alt=""
+                    />
+                    <footer>
+                        <button> Add to Albums </button>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <h5> Album</h5>
+                        <p> Artist Name</p>
+                    </header>
+                    <img
+                        src="https://static.tvtropes.org/pmwiki/pub/images/350x350px-7748e070_axisboldaslove_950.jpeg"
+                        alt=""
+                    />
+                    <footer>
+                        <button> Add to Albums </button>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <h5> Album</h5>
+                        <p> Artist Albums</p>
+                    </header>
+                    <img
+                        src="https://static.tvtropes.org/pmwiki/pub/images/350x350px-7748e070_axisboldaslove_950.jpeg"
+                        alt=""
+                    />
+                    <footer>
+                        <button> Add to Albums </button>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <h5> Album</h5>
+                        <p> Artist Name</p>
+                    </header>
+                    <img
+                        src="https://static.tvtropes.org/pmwiki/pub/images/350x350px-7748e070_axisboldaslove_950.jpeg"
+                        alt=""
+                    />
+                    <footer>
+                        <button> Add to Albums </button>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <h5> Album</h5>
+                        <p> Artist Name</p>
+                    </header>
+                    <img
+                        src="https://static.tvtropes.org/pmwiki/pub/images/350x350px-7748e070_axisboldaslove_950.jpeg"
+                        alt=""
+                    />
+                    <footer>
+                        <button> Add to Albums </button>
+                    </footer>
+                </article>
+               
+            </main>
         </div>
     );
 }
