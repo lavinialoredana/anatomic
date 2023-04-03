52bdea232d9a36096a08d175a23b506712ae1806
lavinia_esp <lavinialoreda@gmail.com>
2023-04-04 00:29:15 +0200

    Apply styling to Albums

--- src/pages/Albums/index.jsx
+++ src/pages/Albums/index.jsx
@@ -2,12 +2,13 @@ import { useContext } from "react";
 import { AllAlbumsContext } from "../../context/AllAlbumsProvider";
 import HeaderBar from "../../components/HeaderBar";
 import Results from "../../components/Results";
+import "./Albums.css";
 
 const Albums = () => {
     const { allAlbums } = useContext(AllAlbumsContext);
 
     return (
-        <div>
+        <div className="Albums-container grid">
             <HeaderBar showSearchBar={false} />
             <Results resultsData={allAlbums} />
         </div>
