5596e50084d53cc8a793c8ca166eedbfd5d338e2
lavinia_esp <lavinialoreda@gmail.com>
2023-04-04 00:31:33 +0200

    Style Buttons in Pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -4,9 +4,11 @@ const Pagination = ({ onPreviousPage, currentPage, onPaginate, onNextPage }) => 
     return (
         <div className="Pagination-container">
             <ul className="Pagination">
-                <li onClick={onPreviousPage}>Prev</li>
+                <li onClick={onPreviousPage} role="button">
+                    Prev
+                </li>
                 <li className="Pagination-number">{currentPage}</li>
-                <li onClick={onNextPage} className="Pagination-number">
+                <li onClick={onNextPage} role="button" className="Pagination-number">
                     Next
                 </li>
             </ul>
